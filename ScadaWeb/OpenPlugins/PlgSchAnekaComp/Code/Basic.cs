b1d78b99cc58bc346cdac12cb30bc829035dca55
Ketut Kumajaya <ketut.kumajaya@gmail.com>
2025-01-29 09:31:37 +0700

    OpenPlugins: Add Aneka scheme components

---
+++ ScadaWeb/OpenPlugins/PlgSchAnekaComp/Code/Basic.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Web.Plugins.PlgScheme.Model;
+using Scada.Web.Plugins.PlgScheme.Model.DataTypes;
+using Scada.Web.Plugins.PlgScheme.Model.PropertyGrid;
+using System.ComponentModel;
+using System.Xml;
+
+namespace Scada.Web.Plugins.PlgSchAnekaComp.Code
+{
+    /// <summary>
+    /// Scheme component that represents a static svg image.
+    /// <para>Компонент схемы, представляющий светодиод.</para>
+    /// </summary>
+    [Serializable]
+    public class Basic : ComponentBase
+    {
+        /// <summary>
+        /// Размер по умолчанию.
+        /// </summary>
+        public static readonly Size DefaultSize = new(32, 32);
+
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public Basic()
+            : base()
+        {
+            //serBinder = PlgUtils.SerializationBinder;
+
+            BackColor = "";
+            BorderColor = "Gray";
+            BorderWidth = 0;
+            FillColor = "Silver";
+            FillOpacity = 85;
+            StrokeColor = "Gray";
+            StrokeOpacity = 100;
+            Size = DefaultSize;
+            ImageName = "";
+            ImageStretch = ImageStretches.Zoom;
+        }
+
+        /// <summary>
+        /// Получить или установить цвет фона.
+        /// </summary>
+        #region Attributes
+        [DisplayName("Fill color"), Category(Categories.Appearance)]
+        [Description("The fill color of the SVG component.")]
+        //[Editor(typeof(ColorEditor), typeof(UITypeEditor))]
+        #endregion
+        public string FillColor { get; set; }
+
+        /// <summary>
+        /// Получить или установить непрозрачность границы.
+        /// </summary>
+        #region Attributes
+        [DisplayName("Fill opacity"), Category(Categories.Appearance)]
+        [Description("The fill opacity percentage of the SVG component.")]
+        #endregion
+        public int FillOpacity { get; set; }
+
+        /// <summary>
+        /// Получить или установить цвет границы.
+        /// </summary>
+        #region Attributes
+        [DisplayName("Stroke color"), Category(Categories.Appearance)]
+        [Description("The stroke color of the SVG component.")]
+        //[Editor(typeof(ColorEditor), typeof(UITypeEditor))]
+        #endregion
+        public string StrokeColor { get; set; }
+
+        /// <summary>
+        /// Получить или установить непрозрачность границы.
+        /// </summary>
+        #region Attributes
+        [DisplayName("Stroke opacity"), Category(Categories.Appearance)]
+        [Description("The stroke opacity percentage of the SVG component.")]
+        #endregion
+        public int StrokeOpacity { get; set; }
+
+        /// <summary>
+        /// Получить или установить наименование изображения
+        /// </summary>
+        #region Attributes
+        [DisplayName("Image"), Category(Categories.Appearance)]
+        [Description("The image from the collection of scheme SVG images.")]
+        //[TypeConverter(typeof(ImageConverter)), Editor(typeof(ImageEditor), typeof(UITypeEditor))]
+        [DefaultValue("")]
+        #endregion
+        public string ImageName { get; set; }
+
+        /// <summary>
+        /// Получить или установить растяжение изображения
+        /// </summary>
+        #region Attributes
+        [DisplayName("Image stretch"), Category(Categories.Appearance)]
+        [Description("Stretch the image.")]
+        [DefaultValue(ImageStretches.None)]
+        #endregion
+        public ImageStretches ImageStretch { get; set; }
+
+        /// <summary>
+        /// Загрузить конфигурацию компонента из XML-узла.
+        /// </summary>
+        public override void LoadFromXml(XmlNode xmlNode)
+        {
+            base.LoadFromXml(xmlNode);
+
+            FillColor = xmlNode.GetChildAsString("FillColor");
+            FillOpacity = xmlNode.GetChildAsInt("FillOpacity");
+            StrokeColor = xmlNode.GetChildAsString("StrokeColor");
+            StrokeOpacity = xmlNode.GetChildAsInt("StrokeOpacity");
+            ImageName = xmlNode.GetChildAsString("ImageName");
+            ImageStretch = xmlNode.GetChildAsEnum<ImageStretches>("ImageStretch");
+        }
+
+        /// <summary>
+        /// Сохранить конфигурацию компонента в XML-узле.
+        /// </summary>
+        public override void SaveToXml(XmlElement xmlElem)
+        {
+            base.SaveToXml(xmlElem);
+
+            xmlElem.AppendElem("FillColor", FillColor);
+            xmlElem.AppendElem("FillOpacity", FillOpacity);
+            xmlElem.AppendElem("StrokeColor", StrokeColor);
+            xmlElem.AppendElem("StrokeOpacity", StrokeOpacity);
+            xmlElem.AppendElem("ImageName", ImageName);
+            xmlElem.AppendElem("ImageStretch", ImageStretch);
+        }
+    }
+}
---
+++ ScadaWeb/OpenPlugins/PlgSchAnekaComp/Code/Level.cs
@@ -0,0 +1,170 @@
+﻿// Copyright (c) Rapid Software LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Scada.Web.Plugins.PlgScheme.Model;
+using Scada.Web.Plugins.PlgScheme.Model.DataTypes;
+using Scada.Web.Plugins.PlgScheme.Model.PropertyGrid;
+using System.ComponentModel;
+using System.Xml;
+
+namespace Scada.Web.Plugins.PlgSchAnekaComp.Code
+{
+    /// <summary>
+    /// Scheme component that represents a level.
+    /// <para>Компонент схемы, представляющий светодиод.</para>
+    /// </summary>
+    [Serializable]
+    public class Level : Basic, IDynamicComponent
+    {
+        /// <summary>
+        /// Размер по умолчанию.
+        /// </summary>
+        public static readonly Size LevelSize = new(16, 64);
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public Level()
+            : base()
+        {
+            //serBinder = PlgUtils.SerializationBinder;
+
+            StrokeColor = "Silver";
+            StrokeOpacity = 85;
+            Minimum = 0;
+            Maximum = 100;
+            Action = Actions.None;
+            Conditions = new List<ColorCondition>();
+            InCnlNum = 0;
+            CtrlCnlNum = 0;
+            Size = LevelSize;
+            ImageStretch = ImageStretches.Fill;
+        }
+
+        /// <summary>
+        /// Получить или установить непрозрачность границы.
+        /// </summary>
+        #region Attributes
+        [DisplayName("Minimum value"), Category(Categories.Appearance)]
+        [Description("The min value of the input channel.")]
+        [DefaultValue(0)]
+        #endregion
+        public int Minimum { get; set; }
+
+        /// <summary>
+        /// Получить или установить непрозрачность границы.
+        /// </summary>
+        #region Attributes
+        [DisplayName("Maximum value"), Category(Categories.Appearance)]
+        [Description("The max value of the input channel.")]
+        [DefaultValue(100)]
+        #endregion
+        public int Maximum { get; set; }
+
+        /// <summary>
+        /// Получить или установить действие.
+        /// </summary>
+        #region Attributes
+        [DisplayName("Action"), Category(Categories.Behavior)]
+        [Description("The action executed by clicking the left mouse button on the component.")]
+        [DefaultValue(Actions.None)]
+        #endregion
+        public Actions Action { get; set; }
+
+        /// <summary>
+        /// Получить условия, определяющие цвет заливки.
+        /// </summary>
+        #region Attributes
+        [DisplayName("Conditions"), Category(Categories.Behavior)]
+        [Description("The conditions determining the fill color depending on the value of the input channel.")]
+        [DefaultValue(null), TypeConverter(typeof(CollectionConverter))]
+        //[Editor(typeof(CollectionEditor), typeof(UITypeEditor))]
+        #endregion
+        public List<ColorCondition> Conditions { get; protected set; }
+
+        /// <summary>
+        /// Получить или установить номер входного канала.
+        /// </summary>
+        #region Attributes
+        [DisplayName("Input channel"), Category(Categories.Data)]
+        [Description("The input channel number associated with the component.")]
+        [DefaultValue(0)]
+        #endregion
+        public int InCnlNum { get; set; }
+
+        /// <summary>
+        /// Получить или установить номер канала управления.
+        /// </summary>
+        #region Attributes
+        [DisplayName("Output channel"), Category(Categories.Data)]
+        [Description("The output channel number associated with the component.")]
+        [DefaultValue(0)]
+        #endregion
+        public int CtrlCnlNum { get; set; }
+
+        /// <summary>
+        /// Загрузить конфигурацию компонента из XML-узла.
+        /// </summary>
+        public override void LoadFromXml(XmlNode xmlNode)
+        {
+            base.LoadFromXml(xmlNode);
+
+            Action = xmlNode.GetChildAsEnum<Actions>("Action");
+
+            XmlNode conditionsNode = xmlNode.SelectSingleNode("Conditions");
+            if (conditionsNode != null)
+            {
+                Conditions = new List<ColorCondition>();
+                XmlNodeList conditionNodes = conditionsNode.SelectNodes("Condition");
+                foreach (XmlNode conditionNode in conditionNodes)
+                {
+                    ColorCondition condition = new ColorCondition { SchemeView = SchemeView };
+                    condition.LoadFromXml(conditionNode);
+                    Conditions.Add(condition);
+                }
+            }
+
+            InCnlNum = xmlNode.GetChildAsInt("InCnlNum");
+            CtrlCnlNum = xmlNode.GetChildAsInt("CtrlCnlNum");
+            Minimum = xmlNode.GetChildAsInt("Minimum");
+            Maximum = xmlNode.GetChildAsInt("Maximum");
+        }
+
+        /// <summary>
+        /// Сохранить конфигурацию компонента в XML-узле.
+        /// </summary>
+        public override void SaveToXml(XmlElement xmlElem)
+        {
+            base.SaveToXml(xmlElem);
+
+            xmlElem.AppendElem("Action", Action);
+
+            XmlElement conditionsElem = xmlElem.AppendElem("Conditions");
+            foreach (Condition condition in Conditions)
+            {
+                XmlElement conditionElem = conditionsElem.AppendElem("Condition");
+                condition.SaveToXml(conditionElem);
+            }
+
+            xmlElem.AppendElem("InCnlNum", InCnlNum);
+            xmlElem.AppendElem("CtrlCnlNum", CtrlCnlNum);
+            xmlElem.AppendElem("Minimum", Minimum);
+            xmlElem.AppendElem("Maximum", Maximum);
+        }
+
+        /// <summary>
+        /// Клонировать объект.
+        /// </summary>
+        public override ComponentBase Clone()
+        {
+            Level clonedComponent = (Level)base.Clone();
+
+            foreach (Condition condition in clonedComponent.Conditions)
+            {
+                condition.SchemeView = SchemeView;
+            }
+
+            return clonedComponent;
+        }
+    }
+}
